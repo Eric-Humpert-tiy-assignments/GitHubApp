95e8546e0e1a2428e53612006912f50809290292
Eric Humpert <EricMac@ricks-mbp.apollosrc.root>
2016-04-25 13:30:02 -0500

    added the filtering functionality

--- scripts.js
+++ scripts.js
@@ -4,9 +4,11 @@ if (this.GitHubRepBrowser === undefined) this.GitHubRepBrowser = {};
 (function(context) {
 
   var list;
+  var input;
+  var data;
 
-  function listBuilder(obj) {
-    for (var val of obj.items){
+  function listBuilder(arr) {
+    for (var val of arr){
       var li = document.createElement('li');
       list.appendChild(li);
       var anchor = document.createElement('a');
@@ -16,15 +18,31 @@ if (this.GitHubRepBrowser === undefined) this.GitHubRepBrowser = {};
     }
   }
 
+  function listFilter() {
+    var query = input.value;
+    var result = [];
+    for (var val of data.items) {
+      if (val.full_name.indexOf(query) > -1) {
+        result.push(val);
+        console.log(result.length);
+      }
+    }
+    list.textContent = '';
+    listBuilder(result);
+  }
+
   function start() {
-    var data = window.githubData;
+    data = window.githubData;
+    input = document.querySelector('#search-box');
+    var button = document.querySelector('#launch-search');
     //Call your code here
     console.log('starting!', context);
     console.log(data);
     list = document.querySelector("#search-results");
-    listBuilder(data);
+    listBuilder(data.items);
 
 
+    button.addEventListener('click', listFilter);
   }
 
   window.GitHubRepBrowser.start = start;
